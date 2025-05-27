8ee258067c7f2422807c5fd7077fdb4ee76530aa
Marina Milo <milomarina51@gmail.com>
2025-05-27 11:47:46 -0300

    feat: add Bagde component

---
+++ frontend/src/lib/components/ui/Bagde.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	export let text: string;
+	export let shape: 'square' | 'circle' = 'square';
+</script>
+
+<div class="flex items-center gap-2">
+	<span
+		class="h-3 w-3 bg-[rgb(var(--color-primary))]"
+		class:rounded-sm={shape === 'square'}
+		class:rounded-full={shape === 'circle'}
+	></span>
+	<p class="text-sm font-semibold text-[rgb(var(--color-primary))]">{text}</p>
+</div>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
 	import Button from '$lib/components/ui/Button.svelte';
 	import Image from '$lib/components/ui/Image.svelte';
+	import Bagde from '$lib/components/ui/Bagde.svelte';
 </script>
 
 <!-- TO-DO: borrar todo esto después, es solo de prueba. Esta es la página HOME o INICIO -->
 <div class="m-4">
 	<h2 class="text-[rgb(var(--base-color))]">Inicio</h2>
 	<h6>Esto es de prueba...</h6>
+
+	<Bagde text="Cada acción cuenta, empezá la tuya"></Bagde>
 
 	<Button label="Botón primario"></Button>
 
