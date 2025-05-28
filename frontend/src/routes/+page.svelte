d06b4d2001214c4306193a84ed484cd89fe2878a
Marina Milo <milomarina51@gmail.com>
2025-05-28 17:20:43 -0300

    refactor: remove unused imports and clean up breadcrumb logic in +page.svelte

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,9 +1,5 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import Button from '$lib/components/ui/Button.svelte';
-	import Image from '$lib/components/ui/Image.svelte';
-	import Bagde from '$lib/components/ui/Badge.svelte';
-	import Ticker from '$lib/components/visual/Ticker.svelte';
 	import HeroSection from '$lib/components/sections/HeroSection.svelte';
 	import { setBreadcrumbs } from '$lib/stores/breadcrumbs';
 
@@ -13,12 +9,14 @@
 		'/instituciones/cruz-roja.png'
 	];
 
-	// Establecer breadcrumbs al montar la página
-	onMount(() => {
-		setBreadcrumbs([
-			{ label: 'Inicio' } // Sin href porque es la página actual
-		]);
-	});
+	// ! No colocaría un breadcrumb en inicio, ya que es la página principal
+
+	// stablecer breadcrumbs al montar la página
+	// onMount(() => {
+	// 	setBreadcrumbs([
+	// 		{ label: 'Inicio' } // Sin href porque es la página actual
+	// 	]);
+	// });
 </script>
 
 <HeroSection />
