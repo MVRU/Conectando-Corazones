1968eb3a9bb8bc5afb8977363ca93603a380e51d
Marina Milo <92191306+MVRU@users.noreply.github.com>
2025-06-05 12:42:47 -0300

    Fix API client base URLs (#11)
    * Fix API client URLs and typo in package description
    
    * chore: configure backend services
    
    * Actualizar .env.example

--- backend/src/firebase/index.ts
+++ backend/src/firebase/index.ts
@@ -2,10 +2,10 @@ import * as admin from 'firebase-admin';
 import { getStorage } from 'firebase-admin/storage';
 import { getAuth } from 'firebase-admin/auth';
 
-// import * as serviceAccount from '../config/firebase.service-account.json'; // TODO: corregir este import para las credenciales JSON de Firebase
+import serviceAccount from '../config/firebase.service-account.json';
 
 admin.initializeApp({
-    // credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
     storageBucket: process.env.FIREBASE_BUCKET,
 });
 
