e021c512b7174d1368184a3d469147ee1084f6fc
Alexis Julián Sklate <asklate@frro.utn.edu.ar>
2025-09-08 10:53:11 -0300

    Se resuelva la cuestion de proyecto-ubicacion-direccion segun issue #34

---
+++ frontend/src/lib/types/Ubicacion.ts
@@ -0,0 +1,11 @@
+export const TIPO_UBICACION = ['principal', 'alternativa', 'virtual'] as const;
+
+export type TipoUbicacion = (typeof TIPO_UBICACION)[number];
+
+export interface Ubicacion {
+  id_ubicacion: number;
+  tipo_ubicacion: TipoUbicacion;
+  que_se_hace: string;
+  direccion_id: number; // FK hacia Direccion
+} 
+
