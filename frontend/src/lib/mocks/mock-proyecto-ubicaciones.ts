8a7a14caf84de4340b82e11947cf55d3bf5f8164
Alexis Julián Sklate <asklate@frro.utn.edu.ar>
2025-12-02 17:15:07 -0300

    Se Agregan nuevas ubicaciones al mock para los proyectos 6,7,8 y 9.

--- frontend/src/lib/mocks/mock-proyecto-ubicaciones.ts
+++ frontend/src/lib/mocks/mock-proyecto-ubicaciones.ts
@@ -50,6 +50,30 @@ const ubicacionesBase: ProyectoUbicacion[] = [
 		ubicacion_id: 7,
 		ubicacion: getUbicacion(7)
 	},
+	{
+		id_proyecto_ubicacion: 9,
+		proyecto_id: 9,
+		ubicacion_id: 8,
+		ubicacion: getUbicacion(8)
+	},
+	{
+		id_proyecto_ubicacion: 10,
+		proyecto_id: 6,
+		ubicacion_id: 4,
+		ubicacion: getUbicacion(4)
+	},
+	{
+		id_proyecto_ubicacion: 11,
+		proyecto_id: 7,
+		ubicacion_id: 2,
+		ubicacion: getUbicacion(2)
+	},
+	{
+		id_proyecto_ubicacion: 12,
+		proyecto_id: 8,
+		ubicacion_id: 6,
+		ubicacion: getUbicacion(6)
+	},
 ];
 
 export const mockProyectoUbicaciones: ProyectoUbicacion[] = ubicacionesBase;
